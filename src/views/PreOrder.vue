155679b01752b4166b0745aa8c124fc99afe4907
Azkya Khan <azkya.khan@hotwax.co>
2022-01-13 18:18:10 +0530

    Internationalized the static code (#1ykzq28)

--- src/views/PreOrder.vue
+++ src/views/PreOrder.vue
@@ -10,65 +10,65 @@
       <main class="content">
         <ion-card>
           <ion-card-header>
-            <ion-card-title>Auto listing</ion-card-title>
+            <ion-card-title>{{ $t("Auto listing") }}</ion-card-title>
           </ion-card-header>
           <ion-item>
-            <ion-label>Automatically list pre-order</ion-label>
+            <ion-label>{{ $t("Automatically list pre-order") }}</ion-label>
             <ion-toggle color="secondary" slot="end" />
           </ion-item>
           <ion-item lines="none">
-            <ion-label class="ion-text-wrap"><p>This will automatically list items from purchase orders for preorder when stock runs out.</p></ion-label>
+            <ion-label class="ion-text-wrap"><p>{{ $t("This will automatically list items from purchase orders for preorder when stock runs out.") }}</p></ion-label>
           </ion-item>
         </ion-card>
 
         <ion-card>
           <ion-card-header>
-            <ion-card-title>Re-allocate pre-orders</ion-card-title>
+            <ion-card-title>{{ $t("Re-allocate pre-orders") }}</ion-card-title>
           </ion-card-header>
           <ion-item>
-            <ion-label>Allocation</ion-label>
-            <ion-button fill="outline" color="secondary" slot="end">Run reallocation</ion-button>
+            <ion-label>{{ $t("Allocation") }}</ion-label>
+            <ion-button fill="outline" color="danger" slot="end">{{ $t("Run reallocation") }}</ion-button>
           </ion-item>
           <ion-item lines="none">
-            <ion-label class="ion-text-wrap"><p>Re-allocation will re-allocate promise dates on all pre-orders based on upcoming inventory from purchase orders. Promise dates that were manually adjusted will be overriden.</p></ion-label>
+            <ion-label class="ion-text-wrap"><p>{{ $t("Re-allocation will re-allocate promise dates on all pre-orders based on upcoming inventory from purchase orders. Promise dates that were manually adjusted will be overriden.") }}</p></ion-label>
           </ion-item>
         </ion-card>
 
         <ion-card>
           <ion-card-header>
-            <ion-card-title>Auto releasing</ion-card-title>
+            <ion-card-title>{{ $t("Auto releasing") }}</ion-card-title>
           </ion-card-header>
           <ion-item>
-            <ion-label>Run daily</ion-label>
+            <ion-label>{{ $t("Run daily") }}</ion-label>
             <ion-checkbox slot="end" />
           </ion-item>
           <ion-item>
-            <ion-label>Release preorders</ion-label>
-            <ion-button fill="outline">Release</ion-button>
+            <ion-label>{{ $t("Release preorders")}}</ion-label>
+            <ion-button fill="outline">{{ $t("Release") }}</ion-button>
           </ion-item>
           <ion-item lines="none">
-            <ion-label class="ion-text-wrap"><p>Auto releasing pre-orders will find pre-orders that have promise dates that have passed and release them from fulfillment.</p></ion-label>
+            <ion-label class="ion-text-wrap"><p>{{ $t("Auto releasing pre-orders will find pre-orders that have promise dates that have passed and release them from fulfillment.") }}</p></ion-label>
           </ion-item>
         </ion-card>
 
         <ion-card>
           <ion-card-header>
-            <ion-card-title>Sync</ion-card-title>
+            <ion-card-title>{{ $t("Sync") }}</ion-card-title>
           </ion-card-header>
           <ion-item>
-            <ion-label>Add pre-order tag in Shopify</ion-label>
+            <ion-label>{{ $t("Add pre-order tag in Shopify") }}</ion-label>
             <ion-checkbox slot="end" />
           </ion-item>
           <ion-item>
-            <ion-label>Add shipping dates in Shopify</ion-label>
+            <ion-label>{{ $t("Add shipping dates in Shopify") }}</ion-label>
             <ion-checkbox slot="end" />
           </ion-item>
           <ion-item>
-            <ion-label>Manual update</ion-label>
-            <ion-button fill="outline">Run sync now</ion-button>
+            <ion-label>{{ $t("Manual update") }}</ion-label>
+            <ion-button fill="outline">{{ $t("Run sync now") }}</ion-button>
           </ion-item>
           <ion-item lines="none">
-            <ion-label class="ion-text-wrap"><p>Transfer pre-order related information to Shopify as tags and meta fields.</p></ion-label>
+            <ion-label class="ion-text-wrap"><p>{{ $t("Transfer pre-order related information to Shopify as tags and meta fields.") }}</p></ion-label>
           </ion-item>
         </ion-card>
       </main>
