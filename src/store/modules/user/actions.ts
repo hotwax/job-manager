f1a310eb052b1b6c2fdd893494d611828f440ac6
Aniket Kumar Pandey <meet.aniket.kumar@gmail.com>
2022-03-16 17:18:54 +0530

    Implemented: functionality to set eCom store from settings page (#238v9jd).

--- src/store/modules/user/actions.ts
+++ src/store/modules/user/actions.ts
@@ -84,7 +84,8 @@ const actions: ActionTree<UserState, RootState> = {
   /**
    * update current eComStore information
    */
-  async setEcomStore({ commit }, payload) {
+  async setEcomStore({ commit, dispatch }, payload) {
+    dispatch("job/clearPendingJobs", null, { root: true })
     commit(types.USER_CURRENT_ECOM_STORE_UPDATED, payload.eComStore);
   },
   /**
