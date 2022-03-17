d467db7a2c7634d0774bd1de18e4f1a70a767178
Disha Talreja <dishatalreja1202@gmail.com>
2022-03-17 17:04:57 +0530

    Displayed fallback string when no frequency found for a job on pipeline view(#23tw3xk)

--- src/views/Pipeline.vue
+++ src/views/Pipeline.vue
@@ -30,7 +30,7 @@
 
             <ion-item>
               <ion-icon slot="start" :icon="timerOutline" />
-              <ion-label>{{ temporalExpr(job.tempExprId) }}</ion-label>
+              <ion-label>{{ job.tempExprId ? temporalExpr(job.tempExprId) : "🙃"  }}</ion-label>
             </ion-item>
 
             <ion-item lines="full">
