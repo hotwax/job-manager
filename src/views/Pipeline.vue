2a10013f1304ea607ef1f950536a5a4aa1242c02
Disha Talreja <dishatalreja1202@gmail.com>
2022-03-23 15:49:04 +0530

    Implemented history segment(#245a0ux)

--- src/views/Pipeline.vue
+++ src/views/Pipeline.vue
@@ -5,11 +5,24 @@
         <ion-menu-button slot="start" />
         <ion-title>{{ $t("Pipeline") }}</ion-title>
       </ion-toolbar>
+
+      <ion-toolbar>
+        <div>
+          <ion-segment v-model="segmentSelected" @ionChange="segmentChanged">
+            <ion-segment-button value="pending">
+              <ion-label>{{ $t("Pending") }}</ion-label>
+            </ion-segment-button>
+            <ion-segment-button value="history">
+              <ion-label>{{ $t("History") }}</ion-label>
+            </ion-segment-button>
+          </ion-segment>
+        </div>
+      </ion-toolbar>
     </ion-header>
     <ion-content>
       <main>
-        <section>
-          <ion-card v-for="job in pendingJobs" :key="job">
+        <section v-if="segmentSelected === 'pending'">
+          <ion-card v-for="job in pendingJobs" :key="job.jobId">
             <ion-item lines="none">
               <ion-label class="ion-text-wrap">
                 <p class="overline">{{ job.parentJobId }}</p>
@@ -42,7 +55,44 @@
             <ion-button color="danger" fill="clear" @click="cancelJob(job.jobId)">{{ $t("Cancel") }}</ion-button>
           </ion-card>
 
-          <ion-infinite-scroll @ionInfinite="loadMoreJobs($event)" threshold="100px" :disabled="!isScrollable">
+          <ion-infinite-scroll @ionInfinite="loadMorePendingJobs($event)" threshold="100px" :disabled="!isScrollable">
+            <ion-infinite-scroll-content loading-spinner="crescent" :loading-text="$t('Loading')"/>
+          </ion-infinite-scroll>
+          
+        </section>
+
+        <section v-if="segmentSelected === 'history'">
+          <ion-card v-for="job in jobHistory" :key="job.jobId">
+            <ion-item lines="none">
+              <ion-label class="ion-text-wrap">
+                <p class="overline">{{ job.parentJobId }}</p>
+                {{ job.jobName }}
+              </ion-label>
+              <ion-badge v-if="job.runTime" color="dark" slot="end">{{ timeTillJob(job.runTime)}}</ion-badge>
+            </ion-item>
+
+            <!-- Will remove it from comment when description is avaiable -->
+            <!-- <ion-item lines="none">
+              {{ getDescription(job.systemJobEnumId) }}
+            </ion-item> -->
+            <ion-item>
+              <ion-icon slot="start" :icon="timeOutline" />
+              <ion-label>{{ job.runTime ? getTime(job.runTime) : "-"  }}</ion-label>
+            </ion-item>
+
+            <ion-item>
+              <ion-icon slot="start" :icon="timerOutline" />
+              <ion-label>{{ job.tempExprId ? temporalExpr(job.tempExprId)?.description : "🙃"  }}</ion-label>
+            </ion-item>
+
+            <ion-item lines="full">
+              <ion-icon slot="start" :icon="codeWorkingOutline" />
+              <ion-label>{{ job.serviceName }}</ion-label>
+            </ion-item>
+
+          </ion-card>
+
+          <ion-infinite-scroll @ionInfinite="loadMoreJobHistory($event)" threshold="100px" :disabled="!isScrollable">
             <ion-infinite-scroll-content loading-spinner="crescent" :loading-text="$t('Loading')"/>
           </ion-infinite-scroll>
           
@@ -54,7 +104,9 @@
 <script lang="ts">
 import { DateTime } from 'luxon';
 import { mapGetters, useStore } from 'vuex'
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from "vue";
+import { showToast } from '@/utils'
+import { translate } from '@/i18n'
 import {
   IonBadge,
   IonButton,
@@ -70,7 +122,9 @@ import {
   IonTitle,
   IonInfiniteScroll,
   IonInfiniteScrollContent,
-  alertController
+  alertController,
+  IonSegment,
+  IonSegmentButton
 } from "@ionic/vue";
 import { codeWorkingOutline, timeOutline, timerOutline } from "ionicons/icons";
 
@@ -90,10 +144,13 @@ export default defineComponent({
     IonToolbar,
     IonTitle,
     IonInfiniteScroll,
-    IonInfiniteScrollContent
+    IonInfiniteScrollContent,
+    IonSegment,
+    IonSegmentButton
   },
   computed: {
     ...mapGetters({
+      jobHistory: 'job/getJobHistory',
       pendingJobs: 'job/getPendingJobs',
       temporalExpr: 'job/getTemporalExpr',
       getDescription: 'job/getDescription',
@@ -109,18 +166,77 @@ export default defineComponent({
       const timeDiff = DateTime.fromMillis(time).diff(DateTime.local());
       return DateTime.local().plus(timeDiff).toRelative();
     },
-    async loadMoreJobs (event: any) {
-      this.getJobs(
+    async loadMoreJobHistory(event: any){
+      this.getJobHistory(
         undefined,
         Math.ceil(this.pendingJobs.length / (process.env.VUE_APP_VIEW_SIZE as any)).toString()
       ).then(() => {
         event.target.complete();
       })
     },
-    async getJobs(vSize: any, vIndex: any) {
+    async loadMorePendingJobs (event: any) {
+      this.getPendingJobs(
+        undefined,
+        Math.ceil(this.pendingJobs.length / (process.env.VUE_APP_VIEW_SIZE as any)).toString()
+      ).then(() => {
+        event.target.complete();
+      })
+    },
+    segmentChanged (e: CustomEvent) {
+      this.segmentSelected = e.detail.value
+      this.segmentSelected === 'pending'
+    },
+    async skipJob (job: any) {
+      const alert = await alertController
+        .create({
+          header: this.$t('Skip job'),
+          message: this.$t('Skipping will run this job at the next occurence based on the temporal expression.'),
+          buttons: [
+            {
+              text: this.$t("Don't skip"),
+              role: 'cancel',
+            },
+            {
+              text: this.$t('Skip'),
+              handler: async () => {
+                let skipTime = {};
+                const integer1 = this.temporalExpr(job.tempExprId).integer1;
+                const integer2 = this.temporalExpr(job.tempExprId).integer2
+                if(integer1 === 12) {
+                  skipTime = { minutes: integer2 }
+                } else if (integer1 === 10) {
+                  skipTime = { hours: integer2 }
+                } else if (integer1 === 5) {
+                  skipTime = { days: integer2 }
+                } else {
+                  showToast(translate("This job schedule cannot be skipped"));
+                  return ;
+                }
+                const time =  DateTime.fromMillis(job.runTime).diff(DateTime.local()).plus(skipTime);  
+                const updatedRunTime = time.toMillis() + DateTime.local().toMillis()
+                const payload = {
+                  'jobId': job.jobId,
+                  'runTime': updatedRunTime,
+                  'systemJobEnumId': job.systemJobEnumId,
+                  'statusId': "SERVICE_PENDING"
+                } as any
+                await this.store.dispatch('job/updateJob', payload);
+                await this.store.dispatch('job/fetchPendingJobs', {eComStoreId: this.getCurrentEComStore.productStoreId, viewIndex: 0})
+              },
+            }
+          ]
+        });
+      return alert.present();
+    },
+    async getPendingJobs(vSize: any, vIndex: any) {
       const viewSize = vSize ? vSize : process.env.VUE_APP_VIEW_SIZE;
       const viewIndex = vIndex ? vIndex : 0;
         await  this.store.dispatch('job/fetchPendingJobs', {eComStoreId: this.getCurrentEComStore.productStoreId, viewSize, viewIndex});
+    },
+    async getJobHistory(vSize: any, vIndex: any) {
+      const viewSize = vSize ? vSize : process.env.VUE_APP_VIEW_SIZE;
+      const viewIndex = vIndex ? vIndex : 0;
+        await  this.store.dispatch('job/fetchJobHistory', {eComStoreId: this.getCurrentEComStore.productStoreId, viewSize, viewIndex});
     },
     async cancelJob(jobId: any){
       const alert = await alertController
@@ -147,15 +263,18 @@ export default defineComponent({
   },
   created() {
     this.store.dispatch('job/fetchPendingJobs', {eComStoreId: this.getCurrentEComStore.productStoreId, viewSize:process.env.VUE_APP_VIEW_SIZE, viewIndex:0});
+    this.store.dispatch('job/fetchJobHistory', {eComStoreId: this.getCurrentEComStore.productStoreId, viewSize:process.env.VUE_APP_VIEW_SIZE, viewIndex:0});
   },
   setup() {
     const store = useStore();
+    const segmentSelected = ref('pending');
 
     return {
       store,
       codeWorkingOutline,
       timeOutline,
-      timerOutline
+      timerOutline,
+      segmentSelected
     };
   }
 });
