8ccd1654597b4e33dee957d10a49be47fa3e4a8d
Disha Talreja <dishatalreja1202@gmail.com>
2022-03-23 14:08:51 +0530

    Updated the position of toast(#23zj8cv)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,7 +14,7 @@ const showToast = async (message: string) => {
     .create({
       message,
       duration: 3000,
-      position: 'top',
+      position: 'bottom',
     })
   return toast.present();
 }
