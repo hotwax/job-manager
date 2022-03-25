88b34c04f6d85df32d172c1805a49f9e0478e6ed
Aditya Sharma <aditya.sharma@hotwax.co>
2022-03-25 13:43:10 +0530

    Fixed: priotity can be null

--- src/views/InitialLoad.vue
+++ src/views/InitialLoad.vue
@@ -267,7 +267,7 @@ export default defineComponent({
         payload['SERVICE_NAME'] = job.serviceName
         payload['SERVICE_TIME'] = job.runTime.toString()
         payload['SERVICE_COUNT'] = '0'
-        payload['SERVICE_PRIORITY'] = job.priority.toString()
+        payload['SERVICE_PRIORITY'] = job.priority ? job.priority.toString() : ""
         payload['jobFields'] = {
           'productStoreId': this.currentEComStore.productStoreId,
           'systemJobEnumId': job.systemJobEnumId,
