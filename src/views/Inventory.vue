ef9fea2115fcaa4d545bd0f0ee11ec9f60382450
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-04-01 13:00:52 +0530

    Implemented: animation on the inventory page(#23zj3yc)

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -8,7 +8,7 @@
     </ion-header>
 
     <ion-content>
-      <main>
+      <main ref="main">
         <section>
           <ion-card>
             <ion-card-header>
@@ -44,7 +44,7 @@
           </ion-card>
         </section>
 
-        <aside class="desktop-only" v-show="currentJob">
+        <aside class="desktop-only" v-show="currentJob" ref="aside">
           <JobDetail :title="title" :job="currentJob" :status="currentJobStatus" :type="freqType" :key="currentJob"/>
         </aside>
       </main>
@@ -54,6 +54,7 @@
 
 <script lang="ts">
 import {
+  createAnimation,
   IonCard,
   IonCardHeader,
   IonCardTitle,
@@ -67,7 +68,7 @@ import {
   IonToggle,
   IonToolbar,
 } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { mapGetters, useStore } from 'vuex';
 import JobDetail from '@/components/JobDetail.vue'
 import { DateTime } from 'luxon';
@@ -96,7 +97,8 @@ export default defineComponent({
       currentJob: '',
       title: '',
       currentJobStatus: '',
-      freqType: ''
+      freqType: '',
+      isJobDetailAnimationCompleted: false
     }
   },
   computed: {
@@ -179,6 +181,26 @@ export default defineComponent({
       this.title = title
       this.currentJobStatus = status
       this.freqType = this.jobFrequencyType[id]
+
+      if (this.currentJob && !this.isJobDetailAnimationCompleted) {
+        const asideAnimation = createAnimation()
+        .addElement(this.aside)
+        .duration(1500)
+        .easing('ease')
+         .keyframes([
+          { offset: 0, flex: '0', opacity: '0' },
+          { offset: 0.5, flex: '1', opacity: '0' },
+          { offset: 1, flex: '1', opacity: '1' }
+        ])
+        const mainAnimation = createAnimation()
+          .addElement(this.main)
+          .duration(500)
+          .fromTo('gap', '0', 'var(--spacer-2xl)');
+        createAnimation()
+          .addAnimation([mainAnimation, asideAnimation])
+          .play();
+        this.isJobDetailAnimationCompleted = true;
+      }
     },
     getTemporalExpression(enumId: string) {
       return this.getTemporalExpr(this.getJobStatus(this.jobEnums[enumId]))?.description ?
@@ -196,7 +218,12 @@ export default defineComponent({
   },
   setup() {
     const store = useStore();
+    const main = ref({} as Element)
+    const aside = ref({} as Element)
+
     return {
+      aside,
+      main,
       store
     }  
   }
