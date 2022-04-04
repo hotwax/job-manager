d8244e7e3c6934a059be3dc88ee93b55586de8ac
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-04-04 13:01:34 +0530

    Improved: code to make the batch model functional(#24wb3cn)

--- src/components/BatchModal.vue
+++ src/components/BatchModal.vue
@@ -6,7 +6,7 @@
           <ion-icon slot="icon-only" :icon="closeOutline" />
         </ion-button>
       </ion-buttons>
-      <ion-title>Batch 2</ion-title>
+      <ion-title>{{ currentBatch?.jobName ? currentBatch?.jobName : $t('New Batch') }}</ion-title>
     </ion-toolbar>
   </ion-header>
 
@@ -17,10 +17,10 @@
     </ion-item>
     <ion-item>
       <ion-label position="fixed">{{ $t("Schedule") }}</ion-label>
-      <ion-datetime display-format="MM/DD/YYYY" placeholder="date picker"></ion-datetime>
+      <ion-datetime :value="currentBatch?.runTime ? getDateTime(currentBatch.runTime) : ''" @ionChange="updateRunTime($event, currentBatch)" display-format="MM/DD/YYYY" placeholder="date picker" />
     </ion-item>    
 
-    <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+    <ion-fab vertical="bottom" horizontal="end" slot="fixed" @click="updateJob()">
       <ion-fab-button>
         <ion-icon :icon="checkmarkDoneOutline" />  
       </ion-fab-button>
@@ -28,11 +28,12 @@
   </ion-content>
 </template>
 
-<script>
+<script lang="ts">
 import {
   IonButtons,
   IonButton,
   IonContent,
+  IonDatetime,
   IonFab,
   IonFabButton,
   IonHeader,
@@ -46,12 +47,15 @@ import {
 } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { closeOutline, checkmarkDoneOutline } from 'ionicons/icons';
+import { mapGetters, useStore } from 'vuex';
+import { DateTime } from 'luxon';
 export default defineComponent({
   name: 'BatchModal',
   components: {
     IonButtons,
     IonButton,
     IonContent,
+    IonDatetime,
     IonFab,
     IonFabButton,
     IonHeader,
@@ -62,15 +66,81 @@ export default defineComponent({
     IonTitle,
     IonToolbar,
   },
+  props: ["id"],
+  data() {
+    return {
+      currentBatch: {} as any
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getJob: 'job/getJob',
+      shopifyConfigId: 'user/getShopifyConfigId',
+      currentEComStore: 'user/getCurrentEComStore'
+    })
+  },
+  mounted() {
+    this.getCurrentBatchInfo();
+  },
   methods: {
+    getCurrentBatchInfo() {
+      this.currentBatch = this.getJob('ping').find((job: any) => job.id === this.id)
+    },
+    getDateTime(time: any) {
+      return DateTime.fromMillis(time)
+    },
     closeModal() {
       modalController.dismiss({ dismissed: true });
     },
+    async updateJob() {
+      const job = this.currentBatch ? this.currentBatch : this.getJob('ping').find((job: any) => job.status === 'SERVICE_DRAFT');
+
+      // TODO: check for parentJobId and jobEnum and handle this values properly
+      const payload = {
+        'systemJobEnumId': job.systemJobEnumId,
+        'statusId': "SERVICE_PENDING",
+        'recurrenceTimeZone': DateTime.now().zoneName
+      } as any
+      if (job?.status === 'SERVICE_DRAFT') {
+        payload['JOB_NAME'] = job.jobName
+        payload['SERVICE_NAME'] = job.serviceName
+        payload['SERVICE_TIME'] = job.runTime ? job.runTime.toString() : ''
+        payload['SERVICE_PRIORITY'] = job.priority ? job.priority.toString() : ''
+        payload['SERVICE_COUNT'] = '0'
+        payload['jobFields'] = {
+          'productStoreId': this.currentEComStore.productStoreId,
+          'systemJobEnumId': job.systemJobEnumId,
+          'tempExprId': 'MIDNIGHT_DAILY',
+          'maxRecurrenceCount': '-1',
+          'parentJobId': job.parentJobId,
+          'runAsUser': 'system', // default system, but empty in run now
+          'recurrenceTimeZone': DateTime.now().zoneName
+        }
+        payload['shopifyConfigId'] = this.shopifyConfigId
+
+        // checking if the runTimeData has productStoreId, and if present then adding it on root level
+        job?.runTimeData?.productStoreId?.length >= 0 && (payload['productStoreId'] = this.currentEComStore.productStoreId)
+
+        this.store.dispatch('job/scheduleService', {...job.runTimeData, ...payload})
+      } else if (job?.status === 'SERVICE_PENDING') {
+        payload['tempExprId'] = 'MIDNIGHT_DAILY'
+        payload['jobId'] = job.id
+        payload['runTime'] = job.runTime
+
+        this.store.dispatch('job/updateJob', payload)
+      }
+    },
+    updateRunTime(ev: CustomEvent, batch: any) {
+      batch['runTime'] = DateTime.fromISO(ev['detail'].value).toMillis()
+    }
   },
   setup() {
+    const store = useStore();
+
     return {
       closeOutline,
-      checkmarkDoneOutline
+      checkmarkDoneOutline,
+      store
     };
   },
 });
