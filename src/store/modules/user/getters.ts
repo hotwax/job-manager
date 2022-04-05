cfaceee0d40a008e6a08616cf675ec0756dbc810
azkyakhan <azkya.khan@hotwax.co>
2022-04-05 19:21:34 +0530

    Reverted unwanted indentation(#257kwwn)

--- src/store/modules/user/getters.ts
+++ src/store/modules/user/getters.ts
@@ -3,29 +3,29 @@ import UserState from './UserState'
 import RootState from '@/store/RootState'
 
 const getters: GetterTree <UserState, RootState> = {
-  isAuthenticated (state) {
-    return !!state.token;
-  },
-  isUserAuthenticated(state) {
-    return state.token && state.current
-  },
-  getUserToken (state) {
-    return state.token
-  },
-  getUserProfile (state) {
-    return state.current
-  },
-  getInstanceUrl (state) {
-    return state.instanceUrl;
-  },
-  getShopifyConfigId (state) {
-    return state.shopifyConfig;
-  },
-  getCurrentEComStore(state) {
-    return state.currentEComStore
-  },
-  getStatusDesc: (state) => (id: string): any => {
-    return state.statusDesc[id];
-  }
+    isAuthenticated (state) {
+      return !!state.token;
+    },
+    isUserAuthenticated(state) {
+      return state.token && state.current
+    },
+    getUserToken (state) {
+      return state.token
+    },
+    getUserProfile (state) {
+      return state.current
+    },
+    getInstanceUrl (state) {
+      return state.instanceUrl;
+    },
+    getShopifyConfigId (state) {
+      return state.shopifyConfig;
+    },
+    getCurrentEComStore(state) {
+      return state.currentEComStore
+    },
+    getStatusDesc: (state) => (id: string): any => {
+      return state.statusDesc[id];
+    }
 }
 export default getters;
