67ad2de85de5e6ff2d6f30808e50346f396fbe46
Disha Talreja <dishatalreja1202@gmail.com>
2022-05-06 16:27:57 +0530

    Removed unwanted action for clearing pending jobs(#2a23fur)

--- src/store/modules/user/actions.ts
+++ src/store/modules/user/actions.ts
@@ -80,8 +80,7 @@ const actions: ActionTree<UserState, RootState> = {
   /**
    * update current eComStore information
    */
-  async setEcomStore({ commit, dispatch }, payload) {
-    dispatch("job/clearPendingJobs", null, { root: true })
+  async setEcomStore({ commit }, payload) {
     commit(types.USER_CURRENT_ECOM_STORE_UPDATED, payload.eComStore);
   },
   /**
