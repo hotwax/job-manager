96733de9620553ff4ff8743574adb3ca5ca162e8
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2022-05-30 18:21:34 +0530

    Improved: naming convenctions

--- src/store/modules/webhook/actions.ts
+++ src/store/modules/webhook/actions.ts
@@ -10,15 +10,15 @@ const actions: ActionTree<WebhookState, RootState> = {
     await WebhookService.fetchShopifyWebhooks(payload).then(resp => {
       if (resp.status === 200 && resp.data.webhooks?.length > 0 && !hasError(resp)) {
         const { webhooks } = JSON.parse(resp.data.webhooks);
-        const webhooksEnums = JSON.parse(process.env?.VUE_APP_WEBHOOK_ENUMS as string) as any
+        const webhookEnums = JSON.parse(process.env?.VUE_APP_WEBHOOK_ENUMS as string) as any
         const topics: any = {}
         webhooks.map((webhook: any) => {
-          if (webhooksEnums['NEW_PRODUCTS'] === webhook.topic) topics['NEW_PRODUCTS'] = webhook
-          if (webhooksEnums['DELETE_PRODUCTS'] === webhook.topic) topics['DELETE_PRODUCTS'] = webhook
-          if (webhooksEnums['NEW_ORDERS'] === webhook.topic) topics['NEW_ORDERS'] = webhook
-          if (webhooksEnums['CANCELLED_ORDERS'] === webhook.topic) topics['CANCELLED_ORDERS'] = webhook
-          if (webhooksEnums['PAYMENT_STATUS'] === webhook.topic) topics['PAYMENT_STATUS'] = webhook
-          if (webhooksEnums['RETURNS'] === webhook.topic) topics['RETURNS'] = webhook
+          if (webhookEnums['NEW_PRODUCTS'] === webhook.topic) topics['NEW_PRODUCTS'] = webhook
+          if (webhookEnums['DELETE_PRODUCTS'] === webhook.topic) topics['DELETE_PRODUCTS'] = webhook
+          if (webhookEnums['NEW_ORDERS'] === webhook.topic) topics['NEW_ORDERS'] = webhook
+          if (webhookEnums['CANCELLED_ORDERS'] === webhook.topic) topics['CANCELLED_ORDERS'] = webhook
+          if (webhookEnums['PAYMENT_STATUS'] === webhook.topic) topics['PAYMENT_STATUS'] = webhook
+          if (webhookEnums['RETURNS'] === webhook.topic) topics['RETURNS'] = webhook
         })
         commit(types.WEBHOOK_UPDATED, topics)
       }
--- src/store/modules/webhook/mutations-types.ts
+++ src/store/modules/webhook/mutations-types.ts
@@ -1,2 +1,2 @@
 export const SN_WEBHOOK = 'webhook'
-export const WEBHOOK_UPDATED = SN_WEBHOOK + 'SET_WEBHOOK'
+export const WEBHOOK_UPDATED = SN_WEBHOOK + '/WEBHOOK_UPDATED'
