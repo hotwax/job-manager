6299b360102a3d36ac2fed98915181659537f903
shashwatbangar <shashwatbangar@gmail.com>
2022-07-22 15:04:22 +0530

    Fixed: Unwanted spacing in webhooks action(#2fz59a8)

--- src/store/modules/webhook/actions.ts
+++ src/store/modules/webhook/actions.ts
@@ -40,7 +40,7 @@ const actions: ActionTree<WebhookState, RootState> = {
 
     try {
       resp = await WebhookService.subscribeWebhook({ shopifyConfigId: this.state.user.shopifyConfig }, id)
-    
+
       if (resp.status == 200 && !hasError(resp)) {
         showToast(translate('Webhook subscribed successfully'))
       } else {
