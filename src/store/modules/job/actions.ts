ae6dc01cafa613eaff7010c205843614ddca6a24
shashwatbangar <shashwatbangar@gmail.com>
2022-08-08 12:47:14 +0530

    Fixed: shopifyConfigId check in runServiceNow payload(#364ttgf)

--- src/store/modules/job/actions.ts
+++ src/store/modules/job/actions.ts
@@ -531,7 +531,7 @@ const actions: ActionTree<JobState, RootState> = {
         'parentJobId': job.parentJobId,
         'recurrenceTimeZone': this.state.user.current.userTimeZone
       },
-      'shopifyConfigId': this.state.user.shopifyConfigId,
+      'shopifyConfigId': job.shopifyConfigId ? job.shopifyConfigId : this.state.user.shopifyConfigId,
       'statusId': "SERVICE_PENDING",
       'systemJobEnumId': job.systemJobEnumId
     } as any
