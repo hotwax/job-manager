01b614e5b7823632c1872cc6dcff77bb3d0a79a3
Aditya Sharma <aditya.sharma@hotwax.co>
2022-08-31 20:13:12 +0530

    Fixed: Not able to open job update section on Pipeline page for some jobs (#2rht6hk) Handled case when freq type is not available in environment file. Set default freq type if doesn't exist .env
    Additional fixes:
    Missing description when navigating from pipeline to Job Configuration. description field missing in field list while fetching pending jobs
    When updating the frequency it is not reflecting on Job Configuration component, current is updated but jobStatus is not updated. Used updated lifecycle hook to reintialise the value
    When skip or cancel job is done for pending jobs, UI flicker occurs. Current is updated whenever skip/cancel action is performed. It should be done only when JobConfiguration component is open and current has some value. Added check to check current before fetching and updating the job.
    Removed some redundant code

--- src/store/modules/job/actions.ts
+++ src/store/modules/job/actions.ts
@@ -187,7 +187,7 @@ const actions: ActionTree<JobState, RootState> = {
         "statusId": "SERVICE_PENDING",
         "systemJobEnumId_op": "not-empty"
       } as any,
-      "fieldList": [ "systemJobEnumId", "runTime", "tempExprId", "parentJobId", "serviceName", "jobId", "jobName", "currentRetryCount", "statusId", "productStoreId", "runtimeDataId" ],
+      "fieldList": [ "systemJobEnumId", "runTime", "tempExprId", "parentJobId", "serviceName", "jobId", "jobName", "currentRetryCount", "statusId", "productStoreId", "runtimeDataId", "description" ],
       "noConditionFind": "Y",
       "viewSize": payload.viewSize,
       "viewIndex": payload.viewIndex,
@@ -381,6 +381,9 @@ const actions: ActionTree<JobState, RootState> = {
         if(jobs.status === 200 && !hasError(jobs) && jobs.data?.docs.length) {
           jobs = jobs.data?.docs;
           const job = jobs.find((job: any) => job?.jobId === payload.jobId);
+          // We are using status field everywhere so whenever we fetch job again status field needs to be updated
+          // TODO Check why status field is used instead of statusId
+          job && (job.status = job.statusId);
           commit(types.JOB_CURRENT_UPDATED, job);
         }
         showToast(translate('Service updated successfully'))
@@ -462,7 +465,7 @@ const actions: ActionTree<JobState, RootState> = {
     commit(types.JOB_UPDATED_BULK, {})
   },
 
-  async skipJob({ commit, dispatch, getters }, job) {
+  async skipJob({ state, commit, dispatch, getters }, job) {
     let skipTime = {};
     const integer1 = getters['getTemporalExpr'](job.tempExprId)?.integer1;
     const integer2 = getters['getTemporalExpr'](job.tempExprId)?.integer2
@@ -487,21 +490,9 @@ const actions: ActionTree<JobState, RootState> = {
     } as any
 
     const resp = await JobService.updateJob(payload)
-    if (resp.status === 200 && !hasError(resp) && resp.data?.successMessage) {
-      let jobs = await dispatch('fetchJobs', {
-        inputFields: {
-          'systemJobEnumId': job.systemJobEnumId,
-          'systemJobEnumId_op': 'equals'
-        }
-      })
-      if (jobs.status === 200 && !hasError(jobs) && jobs.data?.docs.length) {
-        jobs = jobs.data?.docs;
-        const currentJob = jobs.find((currentJob: any) => currentJob?.jobId === job?.jobId);
-        commit(types.JOB_CURRENT_UPDATED, currentJob);
-      }
-      commit(types.JOB_UPDATED, { job });
-    }
-
+    // Fetch and update current only when there is object in current
+    // Skip job can be performed from pipeline page too causing side effects
+    if (state.current && Object.keys(state.current).length) {
     let jobs = await dispatch('fetchJobs', {
       inputFields: {
         'systemJobEnumId': payload.systemJobEnumId,
@@ -513,6 +504,10 @@ const actions: ActionTree<JobState, RootState> = {
       const currentJob = jobs.find((currentJob: any) => currentJob?.jobId === job?.jobId);
       commit(types.JOB_CURRENT_UPDATED, currentJob);
     }
+    }
+    // This is done for batch jobs
+    commit(types.JOB_UPDATED, { job });
+
     return resp;
   },
 
@@ -597,6 +592,9 @@ const actions: ActionTree<JobState, RootState> = {
           commit(types.JOB_UPDATED, { cachedJob });
         }
 
+        // Fetch and update current only when there is object in current
+        // Cancel job can be performed from pipeline page too causing side effects
+        if (state.current && Object.keys(state.current).length) {
         let jobs = await dispatch('fetchJobs', {
           inputFields: {
             'systemJobEnumId': job.systemJobEnumId,
@@ -607,6 +605,7 @@ const actions: ActionTree<JobState, RootState> = {
           jobs = jobs.data?.docs;
           const currentJob = jobs.find((currentJob: any) => currentJob?.systemJobEnumId === job?.systemJobEnumId);
           commit(types.JOB_CURRENT_UPDATED, currentJob);
+        }
         }
         showToast(translate('Service updated successfully'))
       } else {
