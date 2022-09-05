9a40cab5f6d66309c33421661cbfd5310ad5f073
Disha Talreja <dishatalreja1202@gmail.com>
2022-09-05 11:40:38 +0530

    Updated logic to fetch shopify config for backward compatibility(#2r65b1c)

--- src/store/modules/user/actions.ts
+++ src/store/modules/user/actions.ts
@@ -177,9 +177,17 @@ const actions: ActionTree<UserState, RootState> = {
           commit(types.USER_SHOPIFY_CONFIGS_UPDATED, resp.data.docs);
           commit(types.USER_CURRENT_SHOPIFY_CONFIG_UPDATED, resp.data.docs[0]);
         } else {
-          console.error(resp);
-          commit(types.USER_SHOPIFY_CONFIGS_UPDATED, []);
-          commit(types.USER_CURRENT_SHOPIFY_CONFIG_UPDATED, {});
+          // TODO need to remove api call for fetching fetching shopifyConfig, currently kept it for backward compatibility.
+          payload["entityName"] = 'ShopifyConfig';
+          resp = await UserService.getShopifyConfig(payload);
+          if (resp.status === 200 && !hasError(resp) && resp.data?.docs?.length > 0) {
+            commit(types.USER_SHOPIFY_CONFIGS_UPDATED, resp.data.docs);
+            commit(types.USER_CURRENT_SHOPIFY_CONFIG_UPDATED, resp.data.docs[0]);
+          } else {
+            console.error(resp);
+            commit(types.USER_SHOPIFY_CONFIGS_UPDATED, []);
+            commit(types.USER_CURRENT_SHOPIFY_CONFIG_UPDATED, {});
+          }
         }
       } catch (err) {
         console.error(err);
