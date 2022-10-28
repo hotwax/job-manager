c62f71d6848aa06a7426e7739436c3072db169f0
k2maan <ketuman333@gmail.com>
2022-10-28 09:59:26 +0530

    Resolved: timezone issue which was caused due to ISO conversion happening twice (#2tzb5w4)

--- src/components/JobConfiguration.vue
+++ src/components/JobConfiguration.vue
@@ -16,7 +16,7 @@
       <ion-item>
         <ion-icon slot="start" :icon="timeOutline" />
         <ion-label class="ion-text-wrap">{{ $t("Run time") }}</ion-label>
-        <ion-label class="ion-text-wrap" @click="() => isOpen = true" slot="end">{{ currentJob?.runTime ? getTime(currentJob.runTime) : $t('Select run time') }}</ion-label>
+        <ion-label class="ion-text-wrap" @click="() => isOpen = true" slot="end">{{ runTime ? getTime(runTime) : $t('Select run time') }}</ion-label>
         <!-- TODO: display a button when we are not having a runtime and open the datetime component
         on click of that button
         Currently, when mapping the same datetime component for label and button so it's not working so for
@@ -26,9 +26,9 @@
           <ion-content force-overscroll="false">
             <ion-datetime
               hour-cycle="h12"
-              :min="minDateTime"
-              :value="currentJob?.runTime ? getDateTime(currentJob.runTime) : ''"
+              :value="runTime ? getDateTime(runTime) : ''"
               @ionChange="updateRunTime($event, currentJob)"
+              :show-default-buttons="true"
             />
           </ion-content>
         </ion-modal>
@@ -150,8 +150,11 @@ export default defineComponent({
     return {
       isOpen: false,
       jobStatus: this.status,
-      minDateTime: DateTime.now().toISO()
+      runTime: '' as any,
     }
+  },
+  mounted() {
+    this.runTime = this.currentJob?.runTime 
   },
   updated() {
     // When updating the job, the job is fetched again with the latest values
@@ -324,7 +327,14 @@ export default defineComponent({
     },
     updateRunTime(ev: CustomEvent, job: any) {
       if (job) {
-        job.runTime = handleDateTimeInput(ev['detail'].value)
+        const currTime = DateTime.now().toMillis();
+        const setTime = handleDateTimeInput(ev['detail'].value);
+        
+        if(setTime > currTime) {
+          this.runTime = handleDateTimeInput(ev['detail'].value);
+        } else {
+          showToast(translate("Provide a future date and time"))
+        }
       }
     },
     async viewJobHistory(job: any) {
