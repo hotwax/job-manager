d77162b429b3465b6e902dd536e18cd26de87076
shashwatbangar <shashwatbangar@gmail.com>
2022-11-09 15:21:24 +0530

    Revert "Resolved build fail(#2vjxdha-n)"
    This reverts commit dbb3caf66cef3c8759dfa4b1f2f931fa2e396969.

--- src/components/InitialJobConfiguration.vue
+++ src/components/InitialJobConfiguration.vue
@@ -20,6 +20,7 @@
         <ion-modal :is-open="isOpen" @didDismiss="() => isOpen = false">
           <ion-content force-overscroll="false">
             <ion-datetime
+              show-default-buttons
               hour-cycle="h23"
               :value="runTime ? getDateTime(runTime) : ''"
               @ionChange="updateRunTime($event, currentJob)"
