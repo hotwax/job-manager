cca73b8cef6e5d04a1a71900c80865fe4209a4fe
shashwatbangar <shashwatbangar@gmail.com>
2022-12-16 17:14:15 +0530

    Fixed: When updating a job, getCurrent job by matching jobId of currentJob and job we get in response(#32qywdd)

--- src/store/modules/job/actions.ts
+++ src/store/modules/job/actions.ts
@@ -464,7 +464,7 @@ const actions: ActionTree<JobState, RootState> = {
             'systemJobEnumId_op': 'equals'
           }
         })
-        const job = jobs[1];
+        const job = jobs.find((job: any) => job?.jobId === payload.jobId);
         if(job.status === 200 && !hasError(job) && job.data?.docs.length) {
           // We are using status field everywhere so whenever we fetch job again status field needs to be updated
           // TODO Check why status field is used instead of statusId
