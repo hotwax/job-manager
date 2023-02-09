dd7d8d349e63b6d985c7dd5740d07288f4252e9d
Aditya Sharma <aditya.sharma@hotwax.co>
2023-02-09 15:01:30 +0530

    Imporved: removed check for lastShopifyOrderId to be required field for scheduling job (#303)

--- src/components/InitialJobConfiguration.vue
+++ src/components/InitialJobConfiguration.vue
@@ -92,7 +92,7 @@
       </ion-item>
     </ion-list>
 
-    <ion-button size="small" fill="outline" expand="block" :disabled="!hasPermission(Actions.APP_JOB_UPDATE) || !lastShopifyOrderId" @click="runJob('Orders')">{{ $t("Run import") }}</ion-button>
+    <ion-button size="small" fill="outline" expand="block" :disabled="!hasPermission(Actions.APP_JOB_UPDATE)" @click="runJob('Orders')">{{ $t("Run import") }}</ion-button>
   </section>
 </template>
 
