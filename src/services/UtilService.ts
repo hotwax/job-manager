260e87279323712a1833233bd6ad7d73d364976f
amansinghbais <singh11amanbais@gmail.com>
2024-08-14 13:01:50 +0530

    Implemented: logic for job execution timezone management (#719)

--- src/services/UtilService.ts
+++ src/services/UtilService.ts
@@ -1,4 +1,5 @@
 import { api } from '@/adapter';
+import { hasError } from '@hotwax/oms-api';
 
 const getServiceStatusDesc = async (payload: any): Promise<any> => {
   return api({
@@ -9,6 +10,68 @@ const getServiceStatusDesc = async (payload: any): Promise<any> => {
   });
 }
 
+const getProductStoreSetting = async (payload: any): Promise<any> => {
+  return api({
+    url: "performFind",
+    method: "post",
+    data: payload
+  });
+}
+
+const isEnumExists = async (enumId: string): Promise<any> => {
+  try {
+    const resp = await api({
+      url: 'performFind',
+      method: 'POST',
+      data: {
+        entityName: "Enumeration",
+        inputFields: {
+          enumId
+        },
+        viewSize: 1,
+        fieldList: ["enumId"],
+        noConditionFind: 'Y'
+      }
+    }) as any
+
+    if (!hasError(resp) && resp.data.docs.length) {
+      return true
+    }
+    return false
+  } catch (err) {
+    return false
+  }
+}
+
+const createEnumeration = async (payload: any): Promise<any> => {
+  return api({
+    url: "/service/createEnumeration",
+    method: "post",
+    data: payload
+  })
+}
+
+const createProductStoreSetting = async (payload: any): Promise<any> => {
+  return api({
+    url: "service/createProductStoreSetting",
+    method: "post",
+    data: payload
+  });
+}
+
+const updateProductStoreSetting = async (payload: any): Promise<any> => {
+  return api({
+    url: "service/updateProductStoreSetting",
+    method: "post",
+    data: payload
+  });
+}
+
 export const UtilService = {
-  getServiceStatusDesc
+  createEnumeration,
+  createProductStoreSetting,
+  getProductStoreSetting,
+  getServiceStatusDesc,
+  isEnumExists,
+  updateProductStoreSetting
 }
