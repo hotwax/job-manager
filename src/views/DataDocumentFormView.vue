c7e7ab19804531cac78102004793e1ea6f04fe1b
Yash Maheshwari <yashmaheshwari551@gmail.com>
2026-07-03 23:35:35 +0530

    Update src/views/DataDocumentFormView.vue

--- src/views/DataDocumentFormView.vue
+++ src/views/DataDocumentFormView.vue
@@ -710,7 +710,7 @@ const selectEntity = async (entity: string) => {
   if (graph.value && (graph.value.fields.length > 0 || graph.value.conditions.length > 0)) {
     const alert = await alertController.create({
       header: translate("Change Primary Entity?"),
-      message: translate("Changing the Primary Entity will affect your current configuration. What would you like to do?"),
+      message: translate("You already have fields and conditions defined. Changing the primary entity will clear the current configuration. Do you wish to proceed?"),
       buttons: [
         {
           text: translate("Keep Configuration"),
